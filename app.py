90c1fc9641f601b3cbb73de2658e857619445ef6
Chantel Mager <chantelmager@Peters-MacBook-Air-2.local>
2023-02-11 16:05:11 -0500

    api_project

--- app.py
+++ app.py
@@ -1,16 +1,41 @@
+
+from flask import Flask, render_template, request
 import requests
 import json
-response_API = requests.get('https://api.covid19india.org/state_district_wise.json')
-#print(response_API.status_code)
-data = response_API.text
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    response = requests.get('https://api.covid19india.org/state_district_wise.json')
+    data = response.json()
+    districts = []
+    for area in data.keys(): 
+        for district in data[area]['districtData'].keys():
+            districts.append(district)
+
+    return render_template("index.html", districts = districts)
 
 
-parse_json = json.loads(data)
 
 
-active_case = parse_json['Andhra Pradesh']['districtData']['East Godavari']['active']
-# active_case2 = parse_json['Andaman and Nicobar Islands']['districtData']['Jaluan']['active']
+@app.route("/select_district", methods=["POST"])
+def select_district():
+    district = request.form.get("district")
+    response = requests.get('https://api.covid19india.org/state_district_wise.json')
+    data = response.json()
+    print(data)
+   
+    
+    return render_template("selected_district.html", data = data)
 
-print(parse_json)
-print("\n\n\n\n\n")
-print("Active cases in South Andaman:", active_case)
+
+
+# @app.route('/find-data')
+# def find_data():
+    
+#     print (data)
+#     return render_template('index.html' , data = data)
+
+
+if __name__== '__main__':
+    app.run(debug=True, host='0.0.0.0')
